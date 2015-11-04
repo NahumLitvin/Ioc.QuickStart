9200600739314e47e816c7e2ed72385044fd5633
Nahum Litvin <lnahum@gmail.com>
2015-11-04 15:29:45 +0200

    sIMPLE iNJECTOR

--- Ioc.QuickStart/Program.cs
+++ Ioc.QuickStart/Program.cs
@@ -2,6 +2,7 @@
 using Ioc.QuickStart.Concreet;
 using Ioc.QuickStart.Interfaces;
 using Ioc.QuickStart.MongoDAL;
+using SimpleInjector;
 
 namespace Ioc.QuickStart
 {
@@ -9,19 +10,29 @@ namespace Ioc.QuickStart
     {
         static void Main(string[] args)
         {
-            var calc = new Calculator(new MongoInputProvider(new MongoProvider("localhost:\\mongo")), 
-                new MongoOutputProvider(new MongoProvider("localhost:\\mongo")), 
+            // 1. Create a new Simple Injector container
+            var container = new Container();
+
+            // 2. Configure the container (register)
+            container.Register<IMongoProvider, MongoProvider>(Lifestyle.Singleton);
+            container.Register<IInputProvider, MongoInputProvider>();
+            container.Register<IOutputProvider, MongoOutputProvider>();
+            container.Register<IParseProvider,ConsoleParseProvider>();
+            container.Register<IMathProvider, DefaultMathProviderProvider>();
+
+            // 3. Optionally verify the container's configuration.
+            container.Verify();
+
+            /*
+            var calc = new Calculator(
+                new MongoInputProvider(new MongoProvider("localhost:\\mongo")), //<===
+                new MongoOutputProvider(new MongoProvider("localhost:\\mongo")),//<=== 
                 new ConsoleParseProvider(),
                 new DefaultMathProviderProvider()
                 );
 
-          /*  var calc = new Calculator(new ConsoleInputProvider(),
-                new ConsoleOutputProvider(),
-                new ConsoleParseProvider(),
-                new DefaultMathProviderProvider()
-                );*/
-
-
+            */
+            var calc = container.GetInstance<Calculator>();
             calc.MainLoop();
 
             Console.ReadLine();
